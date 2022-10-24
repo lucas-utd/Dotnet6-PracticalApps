0680d3285e9a147756f76bdef1beebe8f9a12526
Lucas Chen <chen@spectralmd.com>
2022-10-23 21:18:57 -0500

    first commit

---
+++ Northwind.Common.DataContext.Sqlite/NorthwindContextExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Packt.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Common.DataContext.Sqlite
+{
+    public static class NorthwindContextExtensions
+    {
+        public static IServiceCollection AddNorthwindContext(
+            this IServiceCollection services, string relativePath = "..")
+        {
+            string databasePath = Path.Combine(relativePath, "Northwind.db");
+
+            services.AddDbContext<NorthwindContext>(options =>
+                options.UseSqlite($"Data Source={databasePath}"));
+            return services;
+        }
+    }
+}
